6d8a8a3b9632da7ab80b0790602f7af83810d4e2
saminakhan999 <samina.khan9999@gmail.com>
2022-04-01 01:02:45 +0100

    changed redirect of /chat settings

--- en_croissant/urls.py
+++ en_croissant/urls.py
@@ -17,11 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='chat/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+     path('', RedirectView.as_view(url='chat/'))
+]
