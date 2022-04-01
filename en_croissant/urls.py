b32e28d18721a6d53bab1f08da94f387ee7ff36f
rwclutterbuck <rwclutterbuck@outlook.com>
2022-04-01 12:50:40 +0100

    Add: backend to urls

--- en_croissant/urls.py
+++ en_croissant/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
+#    path('chat/', include('chat.urls')),
+    path('', include('backend.urls')),
     path('admin/', admin.site.urls)
 ]
