562385754299a4f9054b16bc574e980059ea9b15
saminakhan999 <samina.khan9999@gmail.com>
2022-04-01 11:08:57 +0100

    added dotenv_file for base_dir in settings.py

--- en_croissant/settings.py
+++ en_croissant/settings.py
@@ -13,9 +13,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
