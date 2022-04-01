ef0cbbafbb6e3ec1282c240a5775d4cb9c1b8d53
Samina Khan <67604723+saminakhan999@users.noreply.github.com>
2022-04-01 01:11:09 +0100

    removed redirect to /chat

--- en_croissant/urls.py
+++ en_croissant/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
-     path('', RedirectView.as_view(url='chat/'))
+    path('admin/', admin.site.urls)
 ]
